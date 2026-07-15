397d6e7e0e03e54c1d1cc644b0007cc0dac76dbf
Krish popli <pkrish051@gmail.com>
2026-07-15 18:01:50 +0530

    Time: 1 ms (81.28%) | Memory: 14.9 MB (92.10%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
- int findPairs(vector<int>& nums, int k) {
+    int findPairs(vector<int>& nums, int k) {
 
-    set<pair<int,int>> st;
+        if (k < 0) return 0;
 
-    int n = nums.size();
+        sort(nums.begin(), nums.end());
 
-    for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
+        int n = nums.size();
+        int count = 0;
 
-            if(abs(nums[i] - nums[j]) == k) {
-                st.insert({min(nums[i], nums[j]),
-                           max(nums[i], nums[j])});
-            }
+        for (int i = 0; i < n; i++) {
 
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int target = nums[i] + k;
+
+            if (binary_search(nums.begin() + i + 1, nums.end(), target))
+                count++;
         }
-    }
 
-    return st.size();
-}
+        return count;
+    }
 };
