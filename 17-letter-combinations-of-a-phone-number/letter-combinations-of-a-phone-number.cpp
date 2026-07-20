8a59f1d8fdc3c1a465f5aa4e5eea895f14ba893f
Krish popli <pkrish051@gmail.com>
2026-07-20 16:15:22 +0530

    Time: 3 ms (5.42%) | Memory: 10.3 MB (5.04%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,17 +1,17 @@
  
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {unordered_map<char, string> uo;
-        uo   = {
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"}
-        };
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char, string> uo;
+      
+uo['2'] = "abc";
+uo['3'] = "def";
+uo['4'] = "ghi";
+uo['5'] = "jkl";
+uo['6'] = "mno";
+uo['7'] = "pqrs";
+uo['8'] = "tuv";
+uo['9'] = "wxyz";
         string op="";
         vector<string> res;
         sol(res, op, uo, digits, 0);
