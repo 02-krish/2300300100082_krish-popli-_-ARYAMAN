3af300f7a0c5593eb67af331808a30201e673c66
Krish popli <pkrish051@gmail.com>
2026-08-15 03:04:34 +0530

    Time: 793 ms (5.07%) | Memory: 296.5 MB (13.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -14,7 +14,7 @@ public:
         int take = 0;
         if (prev == -1 || nums[i] > nums[prev])
             take = 1 + solve(i + 1, i, nums, dp);
-
+  notTake = solve(i + 1, prev, nums, dp);
         return dp[i][prev + 1] = max(take, notTake);
     }
 
