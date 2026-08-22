8ef39d4633d0c1cc643c32af499fc7eb3d5f5b45
Krish popli <pkrish051@gmail.com>
2026-08-23 02:31:01 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (78.80%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int jump(vector<int>& arr) {
-        int n = arr.size();
-    int l = 0, r = 0;
-    int jumps = 0;
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+int maxReach = 0;
+int r = 0;
+int l=0;
+int n=nums.size();
+while(r<n-1){
+    for(int i=l;i<=r;i++){
+        maxReach=max(maxReach,nums[i]+i);
+    }
+    l=r+1;
+    r=maxReach;
+    jumps++;
+}
+return jumps;
 
-    while (r < n - 1) {
-        int f = 0;
 
-        for (int i = l; i <= r; i++) {
-            f = max(f, i + arr[i]);
-        }
-
-        l = r + 1;
-        r = f;  // 🔥 IMPORTANT FIX
-        jumps++;
     }
 
-    return jumps;
-    }
 };
